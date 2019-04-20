eaa739667829642a12c9f70a006bb01703b625be
MudCitizen <mudcitizen@gmail.com>
2019-04-20 16:33:00 -0400

    Routing – Redirect

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,14 +1,16 @@
 import { Routes, RouterModule } from "@angular/router";
 import { TableComponent } from "./core/table.component";
 import { FormComponent } from "./core/form.component";
-import { NotFoundComponent } from "./core/notFound.component"; 
+import { NotFoundComponent } from "./core/notFound.component";
 
 // here we see the url / component pairs
 const routes: Routes = [
     { path: "form/:mode/:id", component: FormComponent },
     { path: "form/:mode", component: FormComponent },
-    { path: "", component: TableComponent },
-    { path: "**", component: NotFoundComponent}
+    { path: "does", redirectTo: "/form/create", pathMatch: "prefix" },
+    { path: "table", component: TableComponent },
+    { path: "", redirectTo: "/table", pathMatch: "full" },
+    { path: "**", component: NotFoundComponent }
 ]
 
 // RouterModule.forRoot() creates a Module
