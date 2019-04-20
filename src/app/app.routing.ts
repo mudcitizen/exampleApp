9a6cc1527beadf2dab2c7893e5faf7e4d5b33c9f
MudCitizen <mudcitizen@gmail.com>
2019-04-20 15:51:18 -0400

    Routing – Wildcards

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,15 @@
 import { Routes, RouterModule } from "@angular/router";
 import { TableComponent } from "./core/table.component";
 import { FormComponent } from "./core/form.component";
+import { NotFoundComponent } from "./core/notFound.component"; 
 
 // here we see the url / component pairs
 const routes: Routes = [
     { path: "form/:mode/:id", component: FormComponent },
     { path: "form/:mode", component: FormComponent },
-    { path: "", component: TableComponent }]
+    { path: "", component: TableComponent },
+    { path: "**", component: NotFoundComponent}
+]
 
 // RouterModule.forRoot() creates a Module
 export const routing = RouterModule.forRoot(routes);
