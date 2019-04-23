ffeab7c63338cca3275ffefd64ca5be4b26bfa7a
MudCitizen <mudcitizen@gmail.com>
2019-04-23 19:00:18 -0400

    Routing – Styling Links for Active Routes

--- src/app/core/table.component.ts
+++ src/app/core/table.component.ts
@@ -1,19 +1,36 @@
-import { Component,Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
-import { Observer} from "rxjs";
+import { Observer } from "rxjs";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
     selector: "paTable",
     templateUrl: "table.component.html"
 })
 export class TableComponent {
-    constructor(private model: Model) { }
-    getProduct(key: number): Product {
+
+    private category: string;
+    constructor(private model: Model, activeRoute: ActivatedRoute) {
+        activeRoute.params.subscribe(params => {
+            this.category = params["category"] || null;
+        })
+    } getProduct(key: number): Product {
         return this.model.getProduct(key);
     }
     getProducts(): Product[] {
-        return this.model.getProducts();
+        return this.model.getProducts()
+            .filter(p => this.category == null || p.category == this.category);
+    }
+
+    get categories(): string[] {
+        return this.model.getProducts()
+            .map(p => p.category)
+            /* 
+            array.indexOf(category) returns the index of the 1st occurance. So
+            only the 1st occurances "pass" the filter
+            */ 
+            .filter((category, index, array) => array.indexOf(category) == index);
     }
     deleteProduct(key: number) {
         this.model.deleteProduct(key);
