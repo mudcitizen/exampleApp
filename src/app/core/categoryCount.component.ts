907c3e8aa330616d0633a7aa68e9d55423921a6f
MudCitizen <mudcitizen@gmail.com>
2019-05-07 18:02:52 -0400

    Routing - Accessing Parms from Child Routes

--- src/app/core/categoryCount.component.ts
+++ src/app/core/categoryCount.component.ts
@@ -1,15 +1,23 @@
 import {Component, KeyValueDiffer, KeyValueDiffers, ChangeDetectorRef} from "@angular/core";
+import { Product } from "../model/product.model";
 import { Model } from "../model/repository.model";
+import { _document } from "@angular/platform-browser/src/browser";
 
 @Component({
     selector: "paCategoryCount",
     template: `<div class="bg-primary p-2 text-white">
-                    There are {{count}} categories
+    CategoryCountComponent There are {{count}} categories
                </div>`
 })
 export class CategoryCountComponent {
     private differ: KeyValueDiffer<any, any>;
-    count: number = 0;
+    _count: number = 0;
+    get count() : number { 
+        return this._count;
+    }
+    set count(c : number ) {
+        this._count = c;
+    }
     constructor(private model: Model,
         private keyValueDiffers: KeyValueDiffers,
         private changeDetector: ChangeDetectorRef) { }
@@ -20,7 +28,9 @@ export class CategoryCountComponent {
     }
     ngDoCheck() {
         if (this.differ.diff(this.model.getProducts()) != null) {
-            this.count = this.model.getProducts()
+            let ps : Product[] = this.model.getProducts();
+            console.log(ps);
+            this._count = this.model.getProducts()
                 .map(p => p.category)
                 .filter((category, index, array) => array.indexOf(category) == index)
                 .length;
--- src/app/core/productCount.component.ts
+++ src/app/core/productCount.component.ts
@@ -1,9 +1,10 @@
 import { Component, KeyValueDiffer, KeyValueDiffers, ChangeDetectorRef } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { Model } from "../model/repository.model";
 
 @Component({
     selector: "paProductCount",
-    template: `<div class="bg-info text-white p-2">There are
+    template: `<div class="bg-info text-white p-2">ProductCountComponent There are
                   {{count}} products
                </div>`
 })
@@ -11,10 +12,18 @@ import { Model } from "../model/repository.model";
 export class ProductCountComponent {
     private differ: KeyValueDiffer<any, any>;
     count: number = 0;
+    private category: string;
     constructor(private model: Model,
-        private keyValueDiffers: KeyValueDiffers,
-        private changeDetector: ChangeDetectorRef) { }
-    ngOnInit() {
+            private keyValueDiffers: KeyValueDiffers,
+            private changeDetector: ChangeDetectorRef,
+            activeRoute: ActivatedRoute) {
+        activeRoute.pathFromRoot.forEach(route => route.params.subscribe(params => {
+            if (params["category"] != null) {
+                this.category = params["category"];
+                this.updateCount();
+            }
+        }))
+    }    ngOnInit() {
         this.differ = this.keyValueDiffers
             .find(this.model.getProducts())
             .create();
@@ -25,6 +34,8 @@ export class ProductCountComponent {
         }
     }
     private updateCount() {
-        this.count = this.model.getProducts().length;
+        this.count = this.model.getProducts()
+        .filter(p => this.category == null || p.category == this.category)
+        .length;
     }
 }
