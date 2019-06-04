9114ca3c2acbf26d5276f84ce3fff608e75727e1
MudCitizen <mudcitizen@gmail.com>
2019-06-04 07:07:17 -0400

    Routing - Targeting Named Outlets

--- src/app/ondemand/ondemand.module.ts
+++ src/app/ondemand/ondemand.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OndemandComponent } from "./ondemand.component";
-import { RouterModule } from "@angular/router";          
+import { RouterModule } from "@angular/router";
+import { FirstComponent } from "./first.component";
+import { SecondComponent } from "./second.component";
 
-let routing = RouterModule.forChild([{ path: "", component: OndemandComponent }]);
+let routing = RouterModule.forChild([
+    {
+        path: "",
+        component: OndemandComponent, children: [
+            {
+                path: "",
+                children: [
+                    { outlet: "primary", path: "", component: FirstComponent, },
+                    { outlet: "left", path: "", component: SecondComponent, },
+                    { outlet: "right", path: "", component: SecondComponent, },
+                ]
+            },
+        ]
+    }
+]);
 @NgModule({
-    imports: [CommonModule,routing],
-    declarations: [OndemandComponent],
+    imports: [CommonModule, routing],
+    declarations: [OndemandComponent,FirstComponent,SecondComponent],
     exports: [OndemandComponent]
 })
 export class OndemandModule { }
