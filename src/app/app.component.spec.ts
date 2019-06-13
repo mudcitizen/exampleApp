042d33dc9f7e6cabfdeea51a26e698cd12ab7417
MudCitizen <mudcitizen@gmail.com>
2019-06-13 19:18:09 -0400

    2 specs in a suite

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,3 +1,3 @@
 describe("Jasmine Test Environment", () => {
-  it("is working", () => expect(true).toBe(true));
-});
+  it("test numeric value", () => expect(12).toBeGreaterThan(10));
+  it("test string value", () => expect("London").toMatch("^Lon"));});
